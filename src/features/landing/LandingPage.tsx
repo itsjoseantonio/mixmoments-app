ab5c4ee7c65474b179d2aaf8fbbc44d6ff95aae5
Jose Antonio <jos.valga@gmail.com>
2026-07-17 20:22:19 -0700

    fix: commet unused component

--- src/features/landing/LandingPage.tsx
+++ src/features/landing/LandingPage.tsx
@@ -4,7 +4,7 @@ import { UseCases } from './components/UseCases/UseCases';
 import { Differentiators } from './components/Differentiators/Differentiators';
 import { Story } from './components/Story/Story';
 import { HowItWorks } from './components/HowItWorks/HowItWorks';
-import { Pricing } from './components/Pricing/Pricing';
+// import { Pricing } from './components/Pricing/Pricing';
 import { FinalCta } from './components/FinalCta/FinalCta';
 import { LandingFooter } from './components/LandingFooter/LandingFooter';
 
